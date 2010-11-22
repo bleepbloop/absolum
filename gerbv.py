0c4fc7063fda1488535c3295b8e8b225a1ec1add
Jose Torres <Jose@RampageRobotics.com>
2010-11-22 07:06:40 -0500

    Move Heavily Defined Structures and Working 'Open Layer From File' function

--- gerbv.py
+++ gerbv.py
@@ -20,8 +20,8 @@ class GerberViewer:
     def __init__(self):
         """Import libgerbv"""
         self._libgerbv = CDLL(find_library("gerbv"))
-        self._filename = os.getcwd() \
-                            + "/Py/Absolum/test/ex/ArduinoMp3Shield.drd"
+        self._filename = c_char_p(os.getcwd() \
+                            + '/test/ex/ArduinoMp3Shield.drd')
         self._project = None
 
     def create_project(self):
@@ -29,33 +29,41 @@ class GerberViewer:
         gerbv_create_project = self._libgerbv.gerbv_create_project
         gerbv_create_project.restype = POINTER(GerbvProject)
         self._project = gerbv_create_project()[0]
+        self.print_project()
 
     def open_layer_from_file(self):
         """ Open a file, parse contents, 
             and add a new layer to an existing project."""
         gerbv_open_layer = self._libgerbv.gerbv_open_layer_from_filename
-        #gerbv_open_layer.argtypes = [POINTER(GerbvProject), c_char_p]
-        #gerbv_open_layer(byref(project), filename)
-
-        if self._project != None:
-            self.print_project()
+        gerbv_open_layer.argtypes = [POINTER(GerbvProject), c_char_p]
+        gerbv_open_layer(byref(self._project), self._filename)
 
     def create_rs274x_image_from_file(self):
         pass
 
     def print_project(self):
         print self._project        
-        print "Last Loaded = -1?", self._project.last_loaded
-        print "Check Before Delete = True?", self._project.check_before_delete
-        print "Max Files = 1?", self._project.max_files
+        print "Last Loaded = ", self._project.last_loaded
+        print "Check Before Delete = ", self._project.check_before_delete
+        print "Max Files = ", self._project.max_files
 
 # =============================================================================
 """libgerbv Structures"""
 
+class GerbvLayer(Structure):
+    """ gerbv_layer_t """
+    _fields_ = [("stepAndRepeat", c_int),           # gerbv_step_and_repeat_t
+                ("knockout", c_int),                # gerbv_knockout_t
+                ("rotation", c_double),             # xx gdouble
+                ("polarity", c_int),                # gerbv_polarity_t
+                ("name", c_char_p),                 # xx gchar *
+                ("next", c_void_p)]                 # xx gpointer
+
 class GerbvImage(Structure):
+    """ gerbv_image_t """
     _fields_ = [("layertype", c_int),               # gerbv_layertype_t
                 ("aperature", c_int),               # gerbv_aperature_t *
-                ("layers", c_int),                  # gerbv_layer_t *
+                ("layers", GerbvLayer),             # gerbv_layer_t *
                 ("states", c_int),                  # gerbv_netstate_t *
                 ("amacro", c_int),                  # gerbv_gerbv_amacro_t *
                 ("format", c_int),                  # gerbv_format_t *
@@ -65,20 +73,22 @@ class GerbvImage(Structure):
                 ("drill_stats", c_int)]             # gerbv_drill_status_t *
 
 class GerbvFileInfo(Structure):
-    _fields_ = [("image", c_int),               # gerbv_image_t
+    """ gerbv_fileinfo_t """
+    _fields_ = [("image", GerbvImage),          # xx gerbv_image_t
                 ("color", c_int),               # GdkColor
                 ("alpha", c_uint16),            # xx guint16
                 ("isVisible", c_bool),          # xx gboolean
-                ("privateRenderData", c_int),   # gpointer
+                ("privateRenderData", c_void_p),# xx gpointer
                 ("fullPathname", c_char_p),     # xx gchar *         
                 ("name", c_char_p),             # xx gchar *                
                 ("transform", c_int),           # gerbv_user_transformation_t
                 ("layer_dirty", c_bool)]        # xx gboolean
 
 class GerbvProject(Structure):
+    """ gerbv_project_t """
     _fields_ = [("background", c_int),              # GdkColor
                 ("max_files", c_int),               # xx int
-                ("file", c_int),                    # gerbv_fileinfo_t **
+                ("file", GerbvFileInfo),            # xx gerbv_fileinfo_t **
                 ("curr_index", c_int),              # xx int
                 ("last_loaded", c_int),             # xx int
                 ("renderType", c_int),              # xx int
@@ -92,5 +102,6 @@ class GerbvProject(Structure):
 
 if __name__ == "__main__":
     gerber_viewer = GerberViewer()
+    gerber_viewer.create_project()
     gerber_viewer.open_layer_from_file()
 
