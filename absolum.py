c549e2e75407c7e3add8993f404af1fdfd9b17f1
Jose.Torres <Jose.Torres@8bc.org>
2010-12-04 16:09:41 -0800

    Readding core gerbv functionality

--- absolum.py
+++ absolum.py
@@ -14,7 +14,7 @@ import argparse
 """ Absolum Modules """
 import gerbv
 import gui
-
+from gerbv import gerbv
 
 # =============================================================================
 
@@ -23,12 +23,19 @@ class Absolum:
     def __init__(self):
         pass
 
+    def load_gerber(self):
+    	gerber = gerbv.GerberViewer()
+	print gerber
+
+    def load_gui(self):
+        pass 
+
         
 # =============================================================================
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-             description='Absolum: The Robotics Design Suite')
+             description='Absolum: The Robotic Design Suite')
     args = parser.parse_args()
     absolum = Absolum()
-
+    absolum.load_gerber()
