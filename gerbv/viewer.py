8c4ca7d5890c6ea76a4fc3f272e180e7b64babcd
JoseTorres <jose.torres@8bc.org>
2010-12-04 19:59:19 -0500

    Return a list of tuples for project info.

--- gerbv/viewer.py
+++ gerbv/viewer.py
@@ -10,10 +10,15 @@ Gerber RS-274X, Excellon drill, and CSV pick-and-place files.
 
 __version__ = '0.0.1'
 
+
+""" System Modules """
 import os
 import sys
 from ctypes import *
 from ctypes.util import find_library
+
+""" libgerbv Helper Modules """
+from data import *
 
 # =============================================================================
 class GdkColor(Structure):
@@ -52,98 +57,11 @@ class GerberViewer():
         pass
 
     def print_project(self):
-        print self._project        
-        print "Last Loaded = ", self._project.last_loaded
-        print "Check Before Delete = ", self._project.check_before_delete
-        print "Max Files = ", self._project.max_files
+        return [('project_instance', self._project),        
+       	        ('last_loaded', self._project.last_loaded),
+        		('check_before_delete', self._project.check_before_delete),
+        		('max_files', self._project.max_files)]
 
-# =============================================================================
-"""libgerbv Structures"""
-
-class GerbvLayer(Structure):
-    """ gerbv_layer_t """
-    _fields_ = [("stepAndRepeat", c_int),           # gerbv_step_and_repeat_t
-                ("knockout", c_int),                # gerbv_knockout_t
-                ("rotation", c_double),             # xx gdouble
-                ("polarity", c_int),                # gerbv_polarity_t
-                ("name", c_char_p),                 # xx gchar *
-                ("next", c_void_p)]                 # xx gpointer
-
-class GerbvNetstate(Structure):
-    """ gerbv_netstate_t """
-    _fields_ = [("axisSelect", c_int),              # gerbv_axis_select_t
-                ("mirrorState", c_int),             # gerbv_mirror_state_t
-                ("unit", c_int),                    # gerbv_unit_t
-                ("offsetA", c_double),              # xx gdouble
-                ("offsetB", c_double),              # xx gdouble
-                ("scaleA", c_double),               # xx gdouble
-                ("scaleB", c_double),               # xx gdouble
-                ("next", c_void_p)]                 # xx gpointer
-
-class GerbvNet(Structure):
-    """ gerbv_net_t """
-    pass
-GerbvNet._fields_ = [("start_x", c_double),         # xx double
-                ("start_y", c_double),              # xx double
-                ("stop_x", c_double),               # xx double
-                ("stop_y", c_double),               # xx double
-                ("aperture", c_int),                # xx int
-                ("aperture_state", c_int),          # gerbv_aperture_state_t
-                ("interpolation", c_int),           # gerbv_interpolation_t 
-                ("gerbv_cirseg_t", c_int),          # gerbv_cirseg_t *
-                ("next", POINTER(GerbvNet)),        # xx gerbv_net *
-                ("label", c_void_p),                # GString *
-                ("layer", POINTER(GerbvLayer)),     # xx gerbv_layer_t *
-                ("state", POINTER(GerbvNetstate))]  # xx gerbv_netstate_t *
-
-class GerbvImage(Structure):
-    """ gerbv_image_t """
-    _fields_ = [("layertype", c_int),               # gerbv_layertype_t
-                ("aperture", c_int),                # gerbv_aperature_t *
-                ("layers", GerbvLayer),             # xx gerbv_layer_t *
-                ("states", GerbvNetstate),          # xx gerbv_netstate_t *
-                ("amacro", c_int),                  # gerbv_amacro_t *
-                ("format", c_int),                  # gerbv_format_t *
-                ("info", c_int),                    # gerbv_image_info_t *
-                ("netlist", c_int),                 # gerbv_net_t *
-                ("gerbv_stats", c_int),             # gerbv_stats_t *
-                ("drill_stats", c_int)]             # gerbv_drill_status_t *
-
-class GerbvUserTransformation(Structure):
-    """ gerbv_user_transformation_t """
-    _fields_ = [("translateX", c_double),           # xx gdouble
-                ("translateY", c_double),           # xx gdouble
-                ("scaleX", c_double),               # xx gdouble
-                ("scaleY", c_double),               # xx gdouble
-                ("inverted", c_bool)]               # xx gboolean
-
-class GerbvFileInfo(Structure):
-    """ gerbv_fileinfo_t """
-    _fields_ = [("image", POINTER(GerbvImage)),     # xx gerbv_image_t *
-                ("color", GdkColor),                # xx GdkColor
-                ("alpha", c_uint16),                # xx guint16
-                ("isVisible", c_bool),              # xx gboolean
-                ("privateRenderData", c_void_p),    # xx gpointer
-                ("fullPathname", c_char_p),         # xx gchar *         
-                ("name", c_char_p),                 # xx gchar *                
-                ("transform", GerbvUserTransformation), 
-                    # xx gerbv_user_transformation_t
-                ("layer_dirty", c_bool)]            # xx gboolean
-
-class GerbvProject(Structure):
-    """ gerbv_project_t """
-    _fields_ = [("background", POINTER(GdkColor)),  # xx GdkColor
-                ("max_files", c_int),               # xx int
-                ("file", GerbvFileInfo),            # xx gerbv_fileinfo_t **
-                ("curr_index", c_int),              # xx int
-                ("last_loaded", c_int),             # xx int
-                ("renderType", c_int),              # xx int
-                ("check_before_delete", c_bool),    # xx gboolean
-                ("path", c_char_p),                 # xx gchar *
-                ("execpath", c_char_p),             # xx gchar *
-                ("project", c_char_p)]              # xx gchar *
-
-# =============================================================================
 if __name__ == "__main__":
     gerber_viewer = GerberViewer()
     gerber_viewer.create_project()
