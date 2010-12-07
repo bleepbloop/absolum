706fabd7b7b21f14e0616b3193c464d22cc4c953
Jose Torres <Jose@RampageRobotics.com>
2010-12-07 14:31:31 -0500

    Modules renamed.

--- absolum.py
+++ absolum.py
@@ -5,7 +5,7 @@
 An Electronic Design Automation (EDA) Suite for Robotic Development.
 Currently based on PyGTK & libgerbv.
 
-"What do I do with Absolum"
+"What do I do with Absolum?"
 
 """
 
@@ -17,9 +17,8 @@ import sys
 
 """ Absolum Modules """
 
-from gerbv.gerbv import GerberViewer
-from gui.gui import Gui
-
+from gerbv import viewer
+from gui import window
 
 # =============================================================================
 
@@ -29,13 +28,13 @@ class Absolum():
         print "Welcome to Absolum"
         
     def load_gui(self):
-        self._gui = Gui()
+        self._gui = window.Main()
         
     def start_gui(self):
         self._gui.start()   
         
     def load_gerber(self):
-    	self._gerber_viewer = GerberViewer()
+    	self._gerber_viewer = viewer.GerberViewer()
     	print dir(self._gerber_viewer)
     	
     def connect_atomic(self):
@@ -52,10 +51,9 @@ def main():
     """ Start gui Last """
     absolum.start_gui()
 
-
 if __name__ == "__main__":
-    # print sys.args
-    #parser = argparse.ArgumentParser(
-                 #description='Absolum: The Robotics Design Suite')
-    #args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='Absolum: The Robotics Design Suite')
+    # args = parser.parse_args()
     main()
+    
+    
