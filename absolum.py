9a36df873a6b8b26bab6eeeae7658b2741f5e2cd
JoseTorres <jose.torres@8bc.org>
2010-12-10 00:40:49 -0500

    Minor Comment Changes

--- absolum.py
+++ absolum.py
@@ -23,7 +23,7 @@ from gui import window
 class Absolum():
 
     def __init__(self):
-        print "Welcome to Absolum"
+        print "Welcome to Absolum!"
 
     # Gui 
     def load_gui(self):
@@ -32,23 +32,21 @@ class Absolum():
     def start_gui(self):
         self._gui.start()
 
-    # Gerber
+    # Gerber Viewer
     def load_gerber(self):
         self._gerber_viewer = viewer.GerberViewer()
         print dir(self._gerber_viewer)
 
-    # Database
+    # Databases
     def load_db(self):
         pass
 
-    # Network
+    # Network Connection
     def connect_atomic(self):
         pass
         
-#=============================================================================
->>>>>>> 3390560b27788497f3945e356fc06f3f73b7df38:absolum.py
+# ============================================================================
 
-<<<<<<< HEAD:absolum.py
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
                 description='Absolum: The Robotic Design Suite')
@@ -57,24 +55,13 @@ if __name__ == "__main__":
     # Create Absolum
     absolum = Absolum()
 
-    # Start...  Gerber Viewer
+    # Start...      Gerber Viewer
     absolum.load_gerber()
-    #           GTK Window
+    #               GTK Window
     absolum.load_gui()
 
-    # Init...   Databases
+    # Init...       Databases
     absolum.load_db()
-=======
-    """ Start gui Last """
-    absolum.start_gui()
->>>>>>> 3390560b27788497f3945e356fc06f3f73b7df38:absolum.py
 
-<<<<<<< HEAD:absolum.py
-=======
-if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description='Absolum: The Robotics Design Suite')
-    # args = parser.parse_args()
-    main()
-    
-    
->>>>>>> 3390560b27788497f3945e356fc06f3f73b7df38:absolum.py
+    # Start Last... Gui
+    absolum.start_gui()
