a6fa9b8b5c4f9b8870701fa362883f1b2c56a8d4
JoseTorres <jose.torres@8bc.org>
2010-12-09 23:38:39 -0500

    new db module

--- gui/window.py
+++ gui/window.py
@@ -24,29 +24,27 @@ class Main():
         # Create a new horizontal toolbox
         self.box = gtk.HBox(False, 0)
         self.window.add(self.box)
-		
-		# Second horizontal toolbox
-		self.toolbox = gtk.Hbox(False, 0)
-		self.window.add(self.toolbox)
+
+        # Second horizontal toolbox
+        self.toolbox = gtk.HBox(False, 0)
+        # self.window.add(self.toolbox)
 
         # Create a new parts button
         self.parts_button = gtk.Button("Parts")
         self.box.pack_start(self.parts_button, True, True, 0)
 
-		# Create a new search button
-		self.search_button = gtk.Button("Search")
-		self.box.pack(self.search_button, True, True, 0)
+        # Create a new search button
+        self.search_button = gtk.Button("Search")
+        self.box.pack_start(self.search_button, True, True, 0)
 
         # Show Components
-        self.button.show()
+        self.parts_button.show()
+        # self.search_button.show()
         self.box.show()
         self.window.show()
 
     def start(self):
         gtk.main()
 
-	def 
-
     def stop(self):
         gtk.main_quit()
-
