dc2ebd5db4d36f96b683153cdbfc20fe727e7bae
Jose.Torres <Jose@RampageRobotics.com>
2010-12-15 17:51:27 -0500

    New project bom catalog class

---
+++ project/bom/list.py
@@ -0,0 +1,21 @@
+""" Absolum: project/bom/list.py
+
+    BOM Helper Object
+
+"""
+
+""" System Modules """
+import sqlite3
+
+# ============================================================================
+
+class Catalog(object):
+
+    def __init__(self):
+        self.part_count = 0
+        
+        
+        
+# ============================================================================
+
+    
