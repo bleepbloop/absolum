d4afba730979f5f3b2b3247c8fe2bdf939e1448f
Jose.Torres <Jose@RampageRobotics.com>
2010-12-15 19:44:18 -0500

    New schem/capture Diagram class

---
+++ project/schem/capture.py
@@ -0,0 +1,14 @@
+""" Absolum: project/schem/capture.py
+
+    TODO: Utilize Evince python bindings.
+
+"""
+
+#=============================================================================
+
+class Diagram(object):
+
+    def __init__(self):
+        pass
+        
+#=============================================================================
