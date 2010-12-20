5e24d06d655af54ebec0de407d89b21defc80d96
Jose.Torres <Jose@RampageRobotics.com>
2010-12-20 14:17:58 -0500

    New Atomic Handle Class

--- net/atomic_connect.py
+++ net/atomic_connect.py
@@ -1,10 +1,34 @@
 #! /usr/bin/env python
 
-""" net/sockets.py
+""" Absolum: net/atomic_connect.py
 
-Helper module for custom socket communication.
+    Helper module for custom socket communication.
 
 """
 
+__author__ = 'Jose.Torres@8bc.org (Jose Angel Torres)'
+
+""" System Modules """
+import httplib
+import urllib
+
+""" Absolum Modules """
+
+# ============================================================================
+
+class AtomicHandler(object):
+    
+    def __init__(self):
+        self._data = None
+    
+    def connect(self):
+        conn = httplib.HTTPConnection("1024bit.org")
+        response = conn.getresponse()
+        if response.status == 200:
+            self._data = response.read()
+        return response
+
+#=============================================================================      
+
 if __name__ == "__main__":
-    print "Testing Sockets."
+    print 'Testing Atomic Handle...'
